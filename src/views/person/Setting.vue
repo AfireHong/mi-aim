e425b4a8c0ec853cf660ad5d907905033301d854
AfireHong <varhong2018@gmail.com>
2021-10-26 22:55:56 +0800

    feat: 投递档案

--- src/views/person/Setting.vue
+++ src/views/person/Setting.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>设置</div>
+  <div class="setting">
+    <a-tabs type="card">
+      <a-tab-pane key="1" tab="基本设置"> </a-tab-pane>
+      <a-tab-pane key="2" tab="安全设置"> Content of Tab Pane 2 </a-tab-pane>
+      <a-tab-pane key="3" tab="身份设置"> Content of Tab Pane 3 </a-tab-pane>
+    </a-tabs>
+  </div>
 </template>
 <script>
-export default {};
+export default {
+  setup() {
+    return {};
+  },
+};
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.setting {
+  padding: 20px;
+  background: #fff;
+}
+</style>
